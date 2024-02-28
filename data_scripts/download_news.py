1d676d032864e7d957d196c1d4f38bffe7d47d25
tonyroumi <tonyroumi@gmail.com>
2024-02-27 21:26:32 -0800

    download_news

--- data_scripts/download_news.py
+++ data_scripts/download_news.py
@@ -6,6 +6,22 @@ from bs4 import BeautifulSoup as bs
 import sys
 from PIL import Image
 from io import BytesIO
+import requests
+import lxml
+
+
+def xpath_soup(element):
+   components = []
+   target = element if element.name else element.parent
+   for node in (target, *target.parents)[-2::-1]:  # type: bs4.element.Tag
+      tag = '%s:%s' % (node.prefix, node.name) if node.prefix else node.name
+      siblings = node.parent.find_all(tag, recursive=False)
+      components.append(tag if len(siblings) == 1 else '%s[%d]' % (tag, next(
+            index
+            for index, sibling in enumerate(siblings, 1)
+            if sibling is node
+            )))
+   return '/%s' % '/'.join(components)
 
 # Function to render URLs to file
 def RenderUrlsToFile(urls, output_path, prefix, callbackPerUrl, callbackFinal):
@@ -37,24 +53,90 @@ def RenderUrlsToFile(urls, output_path, prefix, callbackPerUrl, callbackFinal):
     # Function to retrieve DOM tree
     def getDOMTree():
         # Use BeautifulSoup to parse the HTML content
-        soup = bs(driver.page_source, 'html.parser')
-        # Serialize the DOM tree using BeautifulSoup's representation
-        dom_tree = soup.prettify() #Make sure Dom is accesssibile as json 
-        return dom_tree
+        selected_style_props = ['display','visibility','opacity','z-index','background-image','content','image']
 
-    def createNode(element):
-        pass #TODO 
+        def getElements():
+           soup = bs(driver.page_source, 'lxml')
+           return soup.find_all()
+        
+        def createNode(element): #individual element tag and contents
+           node = {}
+           node['name'] = element.name
+        #    node.type = 
+           if(element.string != None):
+              node['value'] = element.string
+           xpath = xpath_soup(element)
+           driver.implicitly_wait(2)
+           try:
+            elem = driver.find_element('xpath', xpath)
+
+        # Find the corresponding WebDriver element using XPath
+
+            computed_style = driver.execute_script('''
+            var elem = arguments[0];
+            var computedStyles = window.getComputedStyle(elem);
+            var selected_style_props = ['display','visibility','opacity','z-index','background-image','content','image'];
+            var styles = {};
+            if(computedStyles) {
+                for(var i=0; i < selected_style_props.length; i++) {
+                    styles[selected_style_props[i]] = computedStyles.getPropertyValue(selected_style_props[i]);
+                }
+            }
+            return styles;''', elem)
+            
+            if(computed_style):
+                node['computed_style'] = computed_style
+            location = elem.location
+            
+            size = elem.size
+            #Position L,T,R,B in pixels 
+            node['position'] = [location['x'], location['y'], location['x']+size['width'], location['y']+size['height']]
+            attrs = driver.execute_script('''
+                var elem = arguments[0];
+                var attributes = elem.attributes;
+                var attributes_dict = {};
+                if(attributes)
+                for (var i = 0; i < attributes.length; i++) {
+                    var attr = attributes[i];
+                    attributes_dict[attr.nodeName] = attr.nodeValue;
+                    }
+                return attributes_dict;''', elem)
+            if(attrs):
+                node['attrs'] = attrs
+           except:
+              node['trash'] = True
+              return node
+           return node
+        
+        element_stack = getElements()
+        processed_stack = []
+
+        while(len(element_stack) != 0):
+           element = element_stack.pop()
+           node = createNode(element)
+           if(len(element.contents) != 0 and element.string == None):
+              node['childNodes']= []
+              for i in element.contents:
+                 if(i.name is not None): ##does not want to include text nodes
+                    childNode = processed_stack.pop() 
+                    node['childNodes'].insert(0, childNode)
+           processed_stack.append(node)
+        return processed_stack.pop()
+        
+           
+                   
+           
     
     # Function to render next URL
     def retrieve():
         nonlocal urlIndex
-        if (len(urls) > 0):
+        if (urlIndex < len(urls)):
             
             url = urls[urlIndex]
             urlIndex += 1
               
             driver.get(url)
-            time.sleep(2)  # Add some delay for page loading (adjust as needed)
+            time.sleep(4) 
               
               # Get paths
             image_path = getImagePath(urlIndex)
@@ -62,25 +144,23 @@ def RenderUrlsToFile(urls, output_path, prefix, callbackPerUrl, callbackFinal):
             dom_tree_path = getDOMPath(urlIndex)
             html_path = getHTMLPath(urlIndex)
             list_path = getListPath()
-            if(driver.current_url == url):
-              # Get HTML and DOM content
-              html_content = driver.page_source
-              dom_tree = getDOMTree()
-
-              # Get screenshot 
-              driver.save_screenshot("screenshot.png")
-              image = Image.open("screenshot.png")
-              os.makedirs('data_news/images', exist_ok=True)
-              image = image.convert("RGB")
-              image.save(image_path, format="JPEG", quality=100) 
-              callbackPerUrl("success", url, getPageID(urlIndex), dom_tree_path, html_path, list_path, dom_tree, html_content)         
+            try:
+               html_content = driver.page_source
+               dom_tree = getDOMTree()
+               driver.save_screenshot("screenshot.png")
+               image = Image.open("screenshot.png")
+               os.makedirs('data_news/images', exist_ok=True)
+               image = image.convert("RGB")
+               image.save(image_path, format="JPEG", quality=100) 
+               callbackPerUrl("success", url, getPageID(urlIndex), dom_tree_path, html_path, list_path, dom_tree, html_content)         
             
-            else:
+            
+            except:
               callbackPerUrl("failure", url, getPageID(urlIndex), dom_tree_path, html_path, list_path, None, None)
-            # Continue with next URL
             retrieve()
         else:
             callbackFinal(driver)
+    
     listPath = getListPath()
     if os.path.exists(listPath):
       os.remove(listPath)
@@ -92,11 +172,11 @@ def callbackPerUrl(status, url, pageID, dom_tree_path, html_path, listPath, dom_
       print(url)
       if not os.path.exists(listPath):
         with open(listPath, 'w') as f:
-          f.write(url)
+          f.write(pageID + "\t" + url)
       else:
         with open(listPath, 'a') as f:
           f.write("\n" + pageID + "\t" + url)
-      dom_content = json.dumps(dom_tree)
+      dom_content = json.dumps(dom_tree, indent=4, default=str)
       with open(dom_tree_path, 'w') as f:
          f.write(dom_content)
       with open(html_path, 'w') as f:
@@ -114,7 +194,7 @@ def callbackFinal(driver):
 if __name__ == "__main__":
     if (len(sys.argv) == 2):
         prefix = sys.argv[1]
-        input_path = "data_news/"+prefix
+        input_path = "data_news/"+prefix+".txt"
         output_path = "data_news"
     else: 
         print("Usage: python download_news.py TRAININGFILENAME")
