dfeb60f8cb9cd99652c59e062750ac4f9fe25fb8
tonyroumi <tonyroumi@gmail.com>
2024-03-05 21:21:48 -0800

    files includes

---
+++ custom_layers/dom_tree.py
@@ -0,0 +1,296 @@
+import os
+import cv2
+import sys
+import json
+import copy
+
+#----- CLASS REPRESENTING DOM TREE
+class DOMTree:
+    def __init__(self, path):
+        #-- LOAD DATA FROM FILE
+        with open(path,'r') as f:
+            root = json.load(f)
+        
+        #-- INIT VARIABLES
+        self.root = root
+
+        #-- Get auxiliary info: parent nodes, local name, get pruned version of tree
+        self.addAdditionalInfo()
+        self.getPrunedVersion()
+
+    def saveTree(self, path):
+        copied_root = copy.deepcopy(self.root)
+
+        # auxiliary fields shall be removed
+        fields_to_remove = ['parent', 'localName', 'prunedChildNodes', 'visibleBox']
+
+        processing_stack = []
+        processing_stack.append(copied_root)
+
+        while len(processing_stack)!=0:
+            node = processing_stack.pop()
+
+            # remove
+            for field in fields_to_remove:
+                if field in node:
+                    del node[field]
+
+            # follow children
+            if 'childNodes' in node:
+                childNodes = node['childNodes']
+                for childNode in childNodes:
+                    processing_stack.append(childNode)
+
+
+        print('Saving tree to '+ str(path))
+        dom_content = json.dumps(copied_root, indent=4, default=str)
+        with open(path, 'w+') as f:
+            f.write(dom_content)
+
+
+    def getLocalNodeName(self, name, count):
+        return name+'['+str(count)+']'
+
+    def getPositionedLeafNodes(self):
+        ## FIND LEAVES WITH POSITION
+        processing_stack = []
+        res = []
+
+        processing_stack.append(self.root)    
+        while len(processing_stack)!=0:
+            node = processing_stack.pop()
+
+            # if it has children follow them
+            if 'childNodes' in node:
+                for childNode in node['prunedChildNodes']:
+                    processing_stack.append(childNode)
+            # if we have not children and element has non zero position
+            else:
+                if 'position' in node and ((node['position'][2]-node['position'][0])*(node['position'][3]-node['position'][1]) != 0):
+                    res.append(node)
+        return res
+
+    def getPaths(self, node):
+        paths = []
+
+        ## if it has some identifier, i.e. unique id or class
+        for elem in node if isinstance(node, (list, tuple)) else [node]:
+            if 'attrs' in elem:
+                attrs = elem['attrs']
+
+                # process IDS
+                if 'id' in attrs:
+                    for _id in attrs['id'].split():
+                        # if id is in uniques
+                        if _id in self.unique_ids:
+                            paths.append(ElementPath(ElementPath.RootType._id, root=_id))
+                    # return paths          
+
+                # process classes
+                if 'class' in attrs:
+                    for _cls in attrs['class'].split():
+                        if _cls in self.unique_classes:
+                            paths.append(ElementPath(ElementPath.RootType._class, root=_cls))
+            
+            ## if it has not parent -> it is a root
+            if 'parent' not in elem:
+                paths.append(ElementPath(ElementPath.RootType._dom_root))
+            
+            
+            else:
+                # get paths to parent
+                parent_paths = self.getPaths(elem['parent'])
+                # append local name to parent path
+                for ppath in parent_paths:
+                    ppath.localNames.append(elem['localName'])
+                    paths.append(ppath)      
+
+        return paths
+
+    def getElementByOneOfPaths(self, paths):
+        node = None
+        for path in paths:
+            node = self.getElementByPath(path)
+            if node is not None:
+                return node
+        return node
+
+
+    def getElementByPath(self, path):
+        # get root node, based on the type
+        if path.root_type == ElementPath.RootType._dom_root:
+            node = self.root
+        elif path.root_type == ElementPath.RootType._class:
+            if path.root in self.unique_classes:
+                node = self.unique_classes[path.root]
+            else:
+                return None
+        elif path.root_type == ElementPath.RootType._id:
+            if path.root in self.unique_ids:
+                node = self.unique_ids[path.root]
+            else:
+                return None
+
+        # follow localNames
+        for localName in path.localNames:
+
+            # if it has no children -> path does not exist
+            if 'childNodes' not in node:
+                return None
+            # process children
+            else:
+                childNamesDict = {}
+                foundMatch = False
+                # for every child
+                for childNode in node['childNodes']:
+                    # if child local name matches with our local name -> follow it
+                    if localName == childNode['localName']:
+                        node = childNode
+                        foundMatch = True
+                        break
+
+                # if there was no match -> path does not exist
+                if not foundMatch:
+                    return None
+
+        # last node is a result
+        return node
+
+    def addAdditionalInfo(self):
+        self.unique_ids = {}
+        self.unique_classes = {} 
+
+        #-- GO THROUGH DATA AND SAVE SOME DATA
+        # initialize processing stack with root
+        processing_stack = []
+        processing_stack.append(self.root)
+        classes_to_delete = set()
+        ids_to_delete = set()
+
+        while len(processing_stack)!=0:
+            node = processing_stack.pop()
+
+            ###------ IDS and Classes
+
+            # if node has some attributes
+            if 'attrs' in node:
+                attrs = node['attrs']
+                # process IDS
+                if 'id' in attrs:
+                    for _id in attrs['id'].split():
+                        # if id is not in uniques
+                        if _id not in self.unique_ids:
+                            self.unique_ids[_id] = node
+                        # if it is there, its not unique -> remove
+                        else:
+                            ids_to_delete.add(_id)
+                            
+                # process classes
+                if 'class' in attrs:
+                    for _cls in attrs['class'].split():
+                        # if class is not in uniques
+                        if _cls not in self.unique_classes:
+                            self.unique_classes[_cls] = node
+                        # if it is there, its not unique -> remove
+                        else:
+                            classes_to_delete.add(_cls)
+
+            ###------ Children processing
+
+            # if node has children, process and follow them
+            if 'childNodes' in node:
+                childNodes = node['childNodes']
+                childNamesDict = {} # init child names
+
+                for childNode in childNodes:
+                    # add parent link
+                    childNode['parent'] = node
+
+                    # add local name
+                    childName = childNode['name']
+                    if childName in childNamesDict:
+                        count = childNamesDict[childName] + 1
+                    else:
+                        count = 1
+                    childNamesDict[childName] = count
+                    localName = self.getLocalNodeName(childName, count)
+                    childNode['localName'] = localName
+
+                    # add to stack for further processing
+                    processing_stack.append(childNode)
+
+        #-- REMOVE NON-UNIQUE CLASSES AND IDS
+        for _cls in classes_to_delete:
+            del self.unique_classes[_cls]
+
+        for _id in ids_to_delete:
+            del self.unique_ids[_id]
+
+        #-- PRINT SIZES OF UNIQUES
+        print("# of unique ids: " + str(len(self.unique_ids)))
+        print("# of unique classes: " + str(len(self.unique_classes)))
+
+    # GETS PRUNED VERSION OF THE TREE        
+    def getPrunedVersion(self):
+        ### REMOVE EMPTY TEXT NODES
+        ### REMOVE ELEMENTS WITH ZERO SIZE (AND WHOLE SUBTREE)
+        ### REMOVE ELEMENTS WHICH ARE HIDDEN (AND WHOLE SUBTREE)
+
+        processing_stack = []
+        processing_stack.append(self.root)    
+
+        while len(processing_stack)!=0:
+            node = processing_stack.pop()
+
+            if 'childNodes' in node:
+                childNodes = node['childNodes']
+                prunedChildNodes = []
+
+                for childNode in childNodes:
+                    ### REMOVE EMPTY TEXT NODES
+                    if ('type' in childNode) and ('value' in childNode) and (not childNode['value'].strip()):
+                        continue
+                    
+                   
+
+                    ### REMOVE ELEMENTS NON TEXT ELEMENTS WHICH DO NOT HAVE COMPUTED STYLE AND USELESS TAGS
+                    if ('type' not in childNode or 'trash' in childNode) and ('computed_style' not in childNode): 
+                        continue
+
+                    ### REMOVE HIDDEN ELEMENTS 
+                    if ('computed_style' in childNode):
+                        if (childNode['computed_style']['display'] == "none" or
+                            childNode['computed_style']['visibility'] == "hidden" or
+                            childNode['computed_style']['opacity'] == "0"):
+                            continue
+
+                    ### ADD TO CHILDREN AND TO PROCESSING STACK
+                    prunedChildNodes.append(childNode)
+                    processing_stack.append(childNode)
+
+                ### SET NEW CHILDREN
+                node['prunedChildNodes'] = prunedChildNodes
+
+#----- CLASS REPRESENTING PATH TO ELEMENT (IN DOM TREE)
+class ElementPath:
+    def __init__(self, root_type, root=None, localNames=None):
+        self.root_type = root_type  # whether use root of tree, or unique class, or unique id
+        self.root = root            # root defines where path starts
+        if localNames is None:
+            localNames = []
+        self.localNames = localNames
+    
+    def __str__(self):
+        if self.root_type!=ElementPath.RootType._dom_root:
+            s = '['+str(self.root_type)+':'+self.root+']'
+        else:
+            s = '['+str(self.root_type)+']'
+        for localName in self.localNames:
+            s = s+'/'+localName
+        return s
+
+
+    class RootType:
+        _dom_root = 1
+        _id = 2
+        _class = 3
---
+++ data_scripts/labelling.py
@@ -0,0 +1,302 @@
+import os
+import cv2
+import sys
+import json
+import copy
+import pickle
+import random
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Add the parent directory to sys.path
+sys.path.append(parent_dir)
+from custom_layers.dom_tree import DOMTree
+import matplotlib.pyplot as plt
+
+
+FIGURE_WIDTH = 14
+FIGURE_HEIGHT = 10
+MAX_LABELED = 500
+
+DOWNLOADED_PAGES_LIST_PATH = 'data_news/downloaded_pages/'
+LABELED_DOM_PATH =  'data_news/labeled_dom_trees/'
+IMAGES_PATH = 'data_news/images/'
+DOM_PATH = 'data_news/dom_trees/'
+PATHS_PATH = 'data_news/element_paths/'
+
+#----- CLASS FOR SELECTING A NODE
+class ElementSelector:
+
+    selected_patch = None
+
+    def __init__(self, image_path, dom_tree):
+        self.image_path = image_path
+        self.dom_tree = dom_tree
+        self.selected_patches = []
+
+    def onPick(self, event):
+        # change back old selected page
+        if event.artist in self.selected_patches:
+            self.selected_patches.remove(event.artist)
+            event.artist.set_linewidth(1)
+        else:
+            self.selected_patches.append(event.artist)
+            event.artist.set_linewidth(4)
+
+       
+
+        event.artist.figure.canvas.draw()
+
+   
+
+
+    def keyPress(self, event):
+        if event.key == 'enter':
+            plt.close()
+
+    def selectElement(self):
+        ## CROP IMAGE
+        crop_top = 900
+        self.fig = plt.figure(figsize=(FIGURE_WIDTH,FIGURE_HEIGHT))
+        im = cv2.imread(self.image_path)
+        
+        im = im[:crop_top,:,:]        
+
+        plt.imshow(cv2.cvtColor(im, cv2.COLOR_BGR2RGB))
+
+        patches = []
+
+         #L,T,R,B in pixels 
+
+        # for each leaf node
+        for leafNode in self.dom_tree.getPositionedLeafNodes():
+            
+            position = leafNode['position']
+
+            # Scale and adjust the coordinates since screenshot taken is fullsize
+            position = [x * 2 for x in position]
+
+            # text nodes have different color (just for sanity checks)
+            patch = plt.Rectangle((position[0], position[1]) ,position[2]-position[0],position[3]-position[1], fill=False, edgecolor='g' if 'type' in leafNode else 'b', linewidth=1, picker=3)
+
+
+            patch.node = leafNode
+
+            # compute size
+            size = (position[2]-position[0])*(position[3]-position[1])
+            # add to patch list
+            patches.append((patch,size))
+
+        #Include later
+        # patches = [(patch, size) for (patch, size) in patches if patch[1] >= self.dom_tree['html']['random_scroll']]  # Filter patches starting below the scroll position
+        patches.sort(key=lambda tup: tup[1], reverse=True)  # sorts in place
+        for (patch,size) in patches:
+            plt.gca().add_patch(patch)
+
+        self.fig.canvas.mpl_connect('pick_event', self.onPick)
+        self.fig.canvas.mpl_connect('key_press_event', self.keyPress)
+        plt.show()
+
+        selected_nodes = [patch.node for patch in self.selected_patches]
+        return selected_nodes
+
+        # if self.selected_patch:
+        #     return self.selected_patch.node
+        # else:
+        #     return None
+
+def findAndLabel(page, authorPaths, datePaths, contentPaths):
+    # get dom
+    dom = getPageDOM(page)
+
+    # find elements
+    authorElement = dom.getElementByOneOfPaths(authorPaths)
+    dateElement = dom.getElementByOneOfPaths(datePaths)
+    contentElement = dom.getElementByOneOfPaths(contentPaths)
+
+    # if we have all elements
+    if authorElement and dateElement and contentElement:
+        authorElement['label'] = 'author'
+        dateElement['label'] = 'date'
+        contentElement['label'] = 'content'
+
+        dom.saveTree(os.path.join(LABELED_DOM_PATH, page+'.json'))
+        return True
+
+    # if we do not have all
+    else:
+        return False
+
+def loadPaths(prefix):
+    print ('Loading paths')
+    authorPaths = []
+    datePaths = []
+    contentPaths = []
+    path_to_saved_path = os.path.join(PATHS_PATH, prefix+'.pkl')
+    if os.path.exists(path_to_saved_path):
+        paths = pickle.load(open(path_to_saved_path,'rb'))
+        authorPaths = paths['author']
+        datePaths = paths['date']
+        contentPaths = paths['content']
+
+    return authorPaths, datePaths, contentPaths
+
+def savePaths(prefix, authorPaths, datePaths, contentPaths):
+    paths = {}
+    paths['author'] = authorPaths
+    paths['date'] = datePaths
+    paths['content'] = contentPaths
+
+    #There are all being written to the same file , not sure if that's what we want to do
+    #Or Paths path will be the same, but different prefixes for different sites
+     
+    pathToSavedPath = os.path.join(PATHS_PATH, prefix+'.pkl')
+    pickle.dump(paths, open(pathToSavedPath,'wb+'))
+
+
+def getUnlabeledPages(pages):
+    print ('Getting unlabed pages')
+
+    unlabeled = []
+    labeledCount=0
+    for page in pages:
+        path = os.path.join(LABELED_DOM_PATH,page+'.json')
+        if os.path.exists(path):
+            labeledCount+=1
+        else:
+            unlabeled.append(page)
+            
+    print('Unlabeled count:'+ str(len(unlabeled)))
+    print('Labeled count:'+ str(labeledCount))
+    return unlabeled
+
+def getPageDOM(page):
+    dom_path = os.path.join(DOM_PATH,page+'.json')
+    return DOMTree(dom_path)
+
+def selectNewPaths(image_path, dom):
+    selector = ElementSelector(image_path,dom)
+    element = selector.selectElement()
+    if element:
+        return dom.getPaths(element)
+    else:
+        return []
+
+
+def getNewPaths(pages, authorPaths, datePaths, contentPaths):
+    updatedPath = False
+
+    # until we have no updated path
+    while not updatedPath:
+        random_page = random.choice(pages)
+        dom = getPageDOM(random_page)
+        page_image_path = os.path.join(IMAGES_PATH,random_page+'.jpeg')
+        displayQuestion=True
+
+        newAuthorPaths = []
+        newDatePaths = []
+        newContentPaths = [] 
+
+        # try to get author
+        authorElement = dom.getElementByOneOfPaths(authorPaths)
+        if authorElement is None and displayQuestion:
+            print('Help me to find the author:')
+            newAuthorPaths = selectNewPaths(page_image_path, dom)
+            if len(newAuthorPaths)>0:
+                updatedPath=True
+            else:
+                displayQuestion=False
+
+        # try to get date
+        dateElement = dom.getElementByOneOfPaths(datePaths)
+        if dateElement is None and displayQuestion:
+            print('Help me to find the date:')
+            newDatePaths = selectNewPaths(page_image_path, dom)
+            if len(newDatePaths)>0:
+                updatedPath=True
+            else:
+                displayQuestion=False
+
+        # try to get content
+        contentElement = dom.getElementByOneOfPaths(contentPaths)
+        if contentElement is None and displayQuestion:
+            print('Help me to find the content:')
+            newContentPaths = selectNewPaths(page_image_path, dom)
+            if len(newContentPaths)>0:
+                updatedPath=True
+            else:
+                displayQuestion=False
+
+    return newAuthorPaths, newDatePaths, newContentPaths
+
+
+if __name__ == "__main__":
+
+    # read params
+    if len(sys.argv) != 2:
+        print('BAD PARAMS. USAGE [prefix]')
+        sys.exit(1)
+    
+    prefix = sys.argv[1]
+
+    # prepare output path
+    if not os.path.exists(LABELED_DOM_PATH):
+        os.makedirs(LABELED_DOM_PATH)
+    if not os.path.exists(PATHS_PATH):
+        os.makedirs(PATHS_PATH)
+
+    # load pages
+    pagesPath = os.path.join(DOWNLOADED_PAGES_LIST_PATH, prefix+ '.txt')
+    with open(pagesPath,'r') as f:
+        pages = [line.split('\t')[0] for line in f.readlines()]
+
+    # try to load paths to elements
+    authorPaths, datePaths, contentPaths = loadPaths(prefix)
+    
+    # split pages to already labeled or unlabeled
+    unlabeledPages = getUnlabeledPages(pages)
+
+    totalLabeledCount = len(pages)-len(unlabeledPages)
+
+    # until there are some unlabeled_pages
+    while len(unlabeledPages)>0:
+
+        # get new paths
+        print('Get new paths')
+        newAuthorPaths, newDatePaths, newContentPaths  = getNewPaths(unlabeledPages, authorPaths, datePaths, contentPaths)
+
+        # update existing paths
+        print('Updating paths')
+        authorPaths.extend(newAuthorPaths)
+        datePaths.extend(newDatePaths)
+        contentPaths.extend(newContentPaths)
+
+        # save new updated paths
+        print('Saving new paths')
+        savePaths(prefix,  authorPaths, datePaths, contentPaths)
+
+        # try to annotate page
+        print('Annotating other pages')
+
+        #succeded_count = 0
+        newUnlabeledPages = []
+
+        for page in unlabeledPages:
+            success = findAndLabel(page, authorPaths, datePaths, contentPaths)
+            if success:
+              print(str(totalLabeledCount) + " " + str(page))
+              totalLabeledCount+=1
+            else:
+                newUnlabeledPages.append(page)
+
+            if totalLabeledCount>=MAX_LABELED:
+                break
+
+        # print result
+        print("Successfully labeled: "+ str(totalLabeledCount) + 'pages.')
+        print("Unlabeled pages: " + str(len(newUnlabeledPages))+ 'pages.')
+   
+        unlabeledPages = newUnlabeledPages
+        
+        # check maximum threshold
+        if totalLabeledCount>=MAX_LABELED:
+            print("Maximum number of labeled examples achieved")
+            break
